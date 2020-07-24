6b66e904a1a1f03132bbf2442b38460d0cd4b960
tangzhaoqian <tangzhaoqian@sunmi.com>
2020-07-24 15:31:03 +0800

    增加工作记录

--- jsonp/expressServer.js
+++ jsonp/expressServer.js
@@ -6,8 +6,9 @@ app.get('/', (req, res, next) => {
     // 读取参数，实际返回给前端的是一段js代码，包含前端回调函数的方法名和需要传给前端的数据
     const { query: { functionName, name } } = req;
     const data = { name };
+    console.log('name', name)
 
-    res.send(`${functionName}(${JSON.stringify(data)})`);
+    res.send(`${functionName}(${JSON.stringify({name})})`);
 });
 
 app.listen(3000, () => {
